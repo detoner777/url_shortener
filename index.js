a01181370b8b4583f089c94f2989f54e047b52d2
Alexey_Lisenko <detoner777@gmail.com>
2019-09-27 17:41:18 +0300

    basic routes create

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+
 connectDB();
 
 app.use(express.json({ extended: false }));
+
+app.use("/", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
 
 const PORT = 5000;
 
