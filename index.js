ec5bd15ac4f3784112081066e592b7e68f143934
Alexey_Lisenko <detoner777@gmail.com>
2019-09-27 17:16:53 +0300

    created baisc express server+conected MongoDB

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
