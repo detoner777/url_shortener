dd1583d77b5bb8180f1c2f1ba3c2d194981b60d2
Alexey_Lisenko <detoner777@gmail.com>
2019-09-28 10:41:56 +0300

    starting working with clien

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import PostUrl from "./components/PostUrl";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>App</p>
+      <PostUrl />
     </div>
   );
 }
