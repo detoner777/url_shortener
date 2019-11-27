ef866077696d551d604cffe4f62119125ca306a6
Alexey_Lisenko <detoner777@gmail.com>
2019-11-27 17:10:31 +0200

    Start working on client Header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PostUrl from "./components/PostUrl";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <PostUrl />
     </div>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Collapse,
+  NavbarToggler
+} from "reactstrap";
+
+export default class Header extends Component {
+  render() {
+    return <div className="navbar-container">adasfa</div>;
+  }
+}
