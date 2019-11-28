a5c4ddafa2eaec3173d9302e36f3e3d384c40585
Alexey_Lisenko <detoner777@gmail.com>
2019-11-28 06:58:14 +0200

    working on Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,78 @@ import {
 } from "reactstrap";
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.closeNav = this.closeNav.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  closeNav() {
+    if (this.state.isOpen === true) {
+      this.toggle();
+    }
+  }
+
   render() {
-    return <div className="navbar-container">adasfa</div>;
+    return (
+      <div className="navbar-container">
+        <Navbar id="white-nav" color="light" light expand="md">
+          <NavbarBrand href="/" className="mr-auto">
+            <Link to="/">
+              <img
+                style={{ width: "90px" }}
+                src="https://clipart.info/images/ccovers/1495749358batman-logo-3d-png-clip-art.png"
+              />
+            </Link>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} className="mr-2" />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav navbar className="ml-auto">
+              <NavItem>
+                <Link to="/" className="padded-nav" onClick={this.closeNav}>
+                  {" "}
+                  HOME{" "}
+                </Link>
+              </NavItem>
+
+              <NavItem>
+                <Link
+                  to="/about"
+                  className="padded-nav"
+                  onClick={this.closeNav}
+                >
+                  {" "}
+                  ABOUT{" "}
+                </Link>
+              </NavItem>
+
+              <NavItem>
+                <a
+                  href="http://fromgaming.com/"
+                  target="_blank"
+                  to="/contact-us"
+                  className="padded-nav"
+                  onClick={this.closeNav}
+                >
+                  {" "}
+                  CONTACT{" "}
+                </a>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
