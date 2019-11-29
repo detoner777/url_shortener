2844a63d944df7a4c641e803143cfe69b7adbe9a
Alexey_Lisenko <detoner777@gmail.com>
2019-11-29 10:59:45 +0200

    Ending component+CSS

---
+++ client/src/components/Ending.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export class Ending extends Component {
+  render() {
+    return (
+      <div className="ending">
+        <div className="quantifiable">
+          <h2>linksPowered</h2>
+          <h3 className="ending-h3">LINKS POWERED BY MEGASHORT</h3>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "../App.css";
+
+export class Home extends Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
